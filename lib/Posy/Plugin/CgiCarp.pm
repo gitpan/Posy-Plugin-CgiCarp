7239e5fd1247d15c8a4fe1511f1ce43b92116e75
Kathryn Andersen <rubykat@cpan.org>
2005-02-06 05:54:12 +0000

    Import of RUBYKAT/Posy-Plugin-CgiCarp-0.61 from CPAN.
    gitpan-cpan-distribution: Posy-Plugin-CgiCarp
    gitpan-cpan-version:      0.61
    gitpan-cpan-path:         RUBYKAT/Posy-Plugin-CgiCarp-0.61.tar.gz
    gitpan-cpan-author:       RUBYKAT
    gitpan-cpan-maturity:     released

--- lib/Posy/Plugin/CgiCarp.pm
+++ lib/Posy/Plugin/CgiCarp.pm
@@ -8,11 +8,11 @@ Posy::Plugin::CgiCarp - Posy plugin to aid debugging by using CGI::Carp.
 
 =head1 VERSION
 
-This describes version B<0.60> of Posy::Plugin::CgiCarp.
+This describes version B<0.61> of Posy::Plugin::CgiCarp.
 
 =cut
 
-our $VERSION = '0.60';
+our $VERSION = '0.61';
 
 =head1 SYNOPSIS
 
@@ -76,6 +76,74 @@ sub debug {
 	carp "DEBUG: ", $message;
     }
 } # debug
+
+=head1 INSTALLATION
+
+Installation needs will vary depending on the particular setup a person
+has.
+
+=head2 Administrator, Automatic
+
+If you are the administrator of the system, then the dead simple method of
+installing the modules is to use the CPAN or CPANPLUS system.
+
+    cpanp -i Posy::Plugin::CgiCarp
+
+This will install this plugin in the usual places where modules get
+installed when one is using CPAN(PLUS).
+
+=head2 Administrator, By Hand
+
+If you are the administrator of the system, but don't wish to use the
+CPAN(PLUS) method, then this is for you.  Take the *.tar.gz file
+and untar it in a suitable directory.
+
+To install this module, run the following commands:
+
+    perl Build.PL
+    ./Build
+    ./Build test
+    ./Build install
+
+Or, if you're on a platform (like DOS or Windows) that doesn't like the
+"./" notation, you can do this:
+
+   perl Build.PL
+   perl Build
+   perl Build test
+   perl Build install
+
+=head2 User With Shell Access
+
+If you are a user on a system, and don't have root/administrator access,
+you need to install Posy somewhere other than the default place (since you
+don't have access to it).  However, if you have shell access to the system,
+then you can install it in your home directory.
+
+Say your home directory is "/home/fred", and you want to install the
+modules into a subdirectory called "perl".
+
+Download the *.tar.gz file and untar it in a suitable directory.
+
+    perl Build.PL --install_base /home/fred/perl
+    ./Build
+    ./Build test
+    ./Build install
+
+This will install the files underneath /home/fred/perl.
+
+You will then need to make sure that you alter the PERL5LIB variable to
+find the modules, and the PATH variable to find the scripts (posy_one,
+posy_static).
+
+Therefore you will need to change:
+your path, to include /home/fred/perl/script (where the script will be)
+
+	PATH=/home/fred/perl/script:${PATH}
+
+the PERL5LIB variable to add /home/fred/perl/lib
+
+	PERL5LIB=/home/fred/perl/lib:${PERL5LIB}
 
 =head1 REQUIRES
 
